47c9dbf701a7f5c7535963ad053c3a8599a061a7
Unidentified :) <56756733+YordanovDnA@users.noreply.github.com>
2025-09-30 20:49:28 +0000

    _01_Exercise - Take two numbers and print their sum

---
+++ src/main/java/com/mycompany/exercises/_01_Exercise_take_two_return_sum.java
@@ -0,0 +1,9 @@
+package com.mycompany.exercises;
+
+public class _01_Exercise_take_two_return_sum {
+    public static void main(String[] args){
+        byte a = 5;
+        byte b = 10;
+        System.out.println(a + " + " + b + " = " + (a+b));
+    }
+}
